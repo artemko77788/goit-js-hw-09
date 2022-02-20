b41fb6b0b4efe7cdf3535d0b543d97109752b773
Artem Mironov <31404136+artemko77788@users.noreply.github.com>
2022-02-20 12:11:25 +0200

    first-task-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+let timerId = null;
+
+refs.buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.buttonStart.setAttribute('disabled', true);
+});
+refs.buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.buttonStart.removeAttribute('disabled');
+});
